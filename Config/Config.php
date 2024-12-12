49be7ad933bb95820a51d8db918d90eee737eb3d
FlavioMer <mereflv17@gmail.com>
2024-12-12 15:07:24 -0600

    Primera Entrega

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/cafeteria/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "cafeteria";
+const CHARSET = "charset=utf8";
+const TITLE = "CAFETERIA";
+const MONEDA = "MXN";
+const CLIENT_ID = "AXekf77a11v21df4h-ufxy-7G0w5DB-oOkbwtnfHUMBcoKApPZxc0RE_Ros-G0tRV3w8wYOgapviHdtr";
+
+const USER_SMTP = "mereflv17@gmail.com";
+const PASS_SMTP = "imcdpxcuhjcozkpn";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
